9da09bd58bde09ae7d2c13bcd52e52600f8865bb
Julien Le Mee <julemee@gmail.com>
2022-08-11 15:30:19 -0400

    rails wtach list challenge

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,10 +4,12 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to @bookmark, notice: "Bookmark was successfully created."
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
