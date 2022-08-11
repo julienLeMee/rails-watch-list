23dad82ae395f18ce96d338e9108a6aa8bbcee5f
Julien Le Mee <julemee@gmail.com>
2022-08-11 14:17:06 -0400

    rails wtach list challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Movie.create!(
+    title: Faker::TvShows::BreakingBad.episode,
+    overview: Faker::JapaneseMedia::OnePiece.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: rand(0..10)
+  )
+end
