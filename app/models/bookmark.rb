4d012c26448ccf9c6b2503adf2545e490de313fd
Julien Le Mee <julemee@gmail.com>
2022-08-11 12:28:13 -0400

    rails wtach list challenge

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }, uniqueness: { scope: [:movie, :list] }
+  validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :movie, scope: :list
 end
