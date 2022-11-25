980ce4d94b3a473dae04be677b5da48c8f229aa6
Julien Le Mee <julemee@gmail.com>
2022-11-25 01:32:18 -0500

    added api for movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_153334) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_050340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,10 +72,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_153334) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "list_id", null: false
+    t.index ["list_id"], name: "index_reviews_on_list_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
+  add_foreign_key "reviews", "lists"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,29 +11,29 @@
 #   )
 # end
 
-# require 'open-uri'
-# require 'json'
+require 'open-uri'
+require 'json'
 
-# puts "Cleaning up database..."
+puts "Cleaning up database..."
 # Movie.destroy_all
-# puts "Database cleaned"
+puts "Database cleaned"
 
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# 10.times do |i|
-#   puts "Importing movies from page #{i + 1}"
-#   movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
-#   movies.each do |movie|
-#     puts "Creating #{movie['title']}"
-#     base_poster_url = "https://image.tmdb.org/t/p/original"
-#     Movie.create(
-#       title: movie['title'],
-#       overview: movie['overview'],
-#       poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-#       rating: movie['vote_average']
-#     )
-#   end
-# end
-# puts "Movies created"
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
+end
+puts "Movies created"
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
